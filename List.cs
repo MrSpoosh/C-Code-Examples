1d5b5375214d5d7206fc9c3fd545c836aa792968
MrSpoosh <155593911+MrSpoosh@users.noreply.github.com>
2024-02-14 21:54:12 -0500

    Update and rename README.md to List.cs

---
+++ List.cs
@@ -0,0 +1,10 @@
+// CS-Code-Examples
+
+// Creates List
+List<string> Values = new List<string>() {"Less", "More", "Equal"};
+
+// Loop Through The List And Print Value
+for (int i = 0; i<Values.Count; i++) {
+    // Code Here
+    Console.WriteLine(Values[i]);
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# C-Code-Examples
-This is just examples i think are useful for being new to C#
