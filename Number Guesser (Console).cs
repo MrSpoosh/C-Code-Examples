2d74e57e2357eeae256511dd3910fbfb19435cda
MrSpoosh <155593911+MrSpoosh@users.noreply.github.com>
2024-02-14 22:39:08 -0500

    Update Number Guesser (Console).cs

--- Number Guesser (Console).cs
+++ Number Guesser (Console).cs
@@ -1,20 +1,36 @@
-public static void Main(string[] args) {
-  int min = 1;
-  int max = 10;
-  Random rand = new Random();
-  int pickedNumber = rand.Next(min, max+1);
-  while (true) {
-    Console.WriteLine("Pick a random number between "+min+" & "+max);
-    string input = Console.ReadLine();
-    int guess = Convert.ToInt32(input);
-    
-    if (guess > pickedNumber) {
-    Console.WriteLine("Less");
-    } else if (guess < pickedNumber) {
-    Console.WriteLine("More");
-    } else {
-    Console.WriteLine("Correct!!");
-    break;
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("What's Your Name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+name+" We are playing a number guessing game");
+            Console.WriteLine("Pick a min number");
+            string minString = Console.ReadLine();
+            Console.WriteLine("Pick a max number");
+            string maxString = Console.ReadLine();
+
+            int min = Convert.ToInt16(minString);
+            int max = Convert.ToInt16(maxString);
+
+            Random rand = new Random();
+            int pickedNumber = rand.Next(min, max+1);
+
+            while (true) {
+                Console.WriteLine("Pick a random number between "+min+" & "+max);
+                string input = Console.ReadLine();
+                int guess = Convert.ToInt16(input);
+
+                if (guess > pickedNumber) {
+                    Console.WriteLine("Less");
+                } else if (guess < pickedNumber) {
+                    Console.WriteLine("More");
+                } else {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+            }
+        }
     }
-  }
 }
